88503e409bf7843aa55dd832ab971a11eb5aaf4c
zhp218 <632369326@qq.com>
2018-04-30 22:24:30 +0800

    2018.04.30 over

--- main.cpp
+++ main.cpp
@@ -4,29 +4,12 @@
 
 int main(){
 
-    int i, j;
-    i = 1;
-    j = 2;
-    int* temp1;
-    int* temp2;
-    temp1 = &i;
-    temp2 = &j;
+    int i = 12;
+    int *pr = & i;
+    *pr = *pr * *pr;
 
-    std::cout << *temp1 << std::endl;
-    std::cout << *temp2 << std::endl;
-
-    // 更改指针的值
-    temp1 = &j;
-    temp2 = &i;
-
-    std::cout << *temp1 << std::endl;
-    std::cout << *temp2 << std::endl;
-
-    // 更改指针指向的值
-    *temp1 = 1;
-    *temp2 = 2;
     std::cout << i << std::endl;
-    std::cout << j << std::endl;
+    std::cout << *pr << std::endl;
 
     return 0;
 }
